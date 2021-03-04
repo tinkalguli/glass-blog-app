5c7abfcff6960489fa6c0811871896a73224b3bc
tinkalguli <tinkalmunu@gmail.com>
2021-03-04 16:24:12 +0530

    Fix minor bug

--- config/passport.js
+++ config/passport.js
@@ -2,71 +2,78 @@ var passport = require("passport");
 var githubStrategy = require("passport-github").Strategy;
 var User = require("../models/user");
 
-var GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://my-glass-app.herokuapp.com/auth/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    var email = profile._json.email;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://my-glass-app.herokuapp.com/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, cb) {
+      console.log(request, accessToken, refreshToken, profile);
+      var email = profile._json.email;
 
-    User.findOne({ email }, (err, user) => {
-        if(err) return cb(err, false);
-        if(!user) {
-            var userInfo = {
-                name : profile.displayName,
-                email : profile._json.email,
-            };
-        
-            User.create(userInfo, (err, user) => {
-                if(err) return cb(err, false);
-                cb(null, user);
-            });
+      User.findOne({ email }, (err, user) => {
+        if (err) return cb(err, false);
+        if (!user) {
+          var userInfo = {
+            name: profile.displayName,
+            email: profile._json.email,
+          };
+
+          User.create(userInfo, (err, user) => {
+            if (err) return cb(err, false);
+            cb(null, user);
+          });
         }
 
         cb(null, user);
-    });
-  }
-));
+      });
+    }
+  )
+);
 
-passport.use( new githubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "https://my-glass-app.herokuapp.com/auth/github/callback"
-}, (accessToken, refreshToken, profile, cb) => {
-    console.log(accessToken, profile);
-    var email = profile._json.email;
+passport.use(
+  new githubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "https://my-glass-app.herokuapp.com/auth/github/callback",
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(accessToken, profile);
+      var email = profile._json.email;
 
-    User.findOne({ email }, (err, user) => {
-        if(err) return cb(err, false);
-        if(!user) {
-            var userInfo = {
-                name : profile.displayName,
-                email : profile._json.email
-            };
-        
-            User.create(userInfo, (err, user) => {
-                if(err) return cb(err, false);
-                cb(null, user);
-            });
+      User.findOne({ email }, (err, user) => {
+        if (err) return cb(err, false);
+        if (!user) {
+          var userInfo = {
+            name: profile.displayName,
+            email: profile._json.email,
+          };
+
+          User.create(userInfo, (err, user) => {
+            if (err) return cb(err, false);
+            cb(null, user);
+          });
         }
 
         cb(null, user);
-    });
-
-}));
+      });
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        if(err) return done(err, false);
-        done(null, user);
-    });
+  User.findById(id, (err, user) => {
+    if (err) return done(err, false);
+    done(null, user);
+  });
 });
